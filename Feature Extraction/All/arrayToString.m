dc8873bd7d786a9f447559e28b8ec99b7da6c761
bjbuter <bjbuter@gmail.com>
2010-06-17 11:01:00 +0000

    more cleanup in all

--- Feature Extraction/All/arrayToString.m
+++
@@ -1,6 +0,0 @@
-function stringVersion = arrayToString(array)
-    stringVersion = num2str(array(1));
-    for i = 2:size(array,2)
-        stringVersion = [stringVersion,',',num2str(array(i))];
-    end
-end
