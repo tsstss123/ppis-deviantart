95ae43f2e5564a215d026dd17df3f5b07f3aa678
bjbuter@gmail.com <bjbuter@gmail.com@6479cd68-e7ad-26bc-137c-6250a2daf512>
2010-06-17 06:17:55 +0000

    moved Weibull

--- trunk/Feature Extraction/All/demoFilter.m
+++
@@ -1,7 +0,0 @@
-im =  im2double(rgb2gray (imread('pic.jpg')));
-
-fprintf('Estimated parameters of integrated Weibull distribution:\n');
-[scaleHist shapeHist locationHist] = integWeibullMle(im, 'x', 1, 1); % for histogram
-fprintf('scale = %g, shape = %g, location = %g for histogram of data\n', scaleHist, shapeHist, locationHist);
-[scale shape location] = integWeibullMle(im, 'x', 0, 1); % for raw data 
-fprintf('scale = %g, shape = %g, location = %g for raw data\n', scale, shape, location);
