0d13a6694f2acc40d9ca0102383e403f5fd85f53
bvandepoel <bvandepoel@6479cd68-e7ad-26bc-137c-6250a2daf512>
2010-06-17 12:24:40 +0000

    Rewrote extractfeature so that it does color conversions only once

--- Feature Extraction/All/intEntropy.m
+++ Feature Extraction/All/intEntropy.m
@@ -1,8 +1,8 @@
-function intEnt = intEntropy(image)
-    if size(image,3) == 3
-       greyImage = rgb2gray(image);
+function intEnt = intEntropy(greyImage)
+%     if size(image,3) == 3
+%        greyImage = rgb2gray(image);
        intEnt = entropy(greyImage);
-    else
-       intEnt = entropy(image(:,:,1)); 
-    end
+%     else
+%        intEnt = entropy(image(:,:,1)); 
+%     end
 end
--- Feature Extraction/All/intVariance.m
+++ Feature Extraction/All/intVariance.m
@@ -1,7 +1,7 @@
-function intVar = intVariance(image)
-    if size(image,3) == 3
-        intVar = variance(rgb2gray(image));
-    else
-        intVar = variance(image(:,:,1));
-    end
+function intVar = intVariance(greyImage)
+%     if size(image,3) == 3
+        intVar = variance(greyImage);
+%     else
+%         intVar = variance(image(:,:,1));
+%     end
 end
