d7180713956962f562c46b7ddc53c0ec4e126416
vannoortsander <vannoortsander@6479cd68-e7ad-26bc-137c-6250a2daf512>
2010-06-18 11:36:55 +0000

    Changed category_analyzer to have options to show per user or total and to show the subcategories.

--- scraper/category_analyzer.py
+++ scraper/category_analyzer.py
@@ -1,3 +1,5 @@
+""" Simple script do count all categories per user
+	Usage: category_analyzer image_folder_path printperuser=True/False printsubcategories=True/False """
 import sys
 import os
 
@@ -5,32 +7,69 @@ import xml.dom.minidom
 from collections import defaultdict
 from operator import itemgetter
 
+def createSubCategory(depth=6):
+	if depth == 0:
+		return [0, None] 
+	return defaultdict( lambda : [0, createSubCategory(depth-1)] )
+
+totalcategories = defaultdict( lambda : [0, createSubCategory()] )
+
+printsubcategories = True
+printperuser = False
+
 def reportDeviant(folder_name, deviant):
-	print('Analyzing deviant %s' % (deviant))
+	global totalcategories
+	#print('Analyzing deviant %s' % (deviant))
 	deviant_folder = os.path.join(folder_name, deviant)
-	categories = defaultdict( lambda : 0 )
+	usercategories = defaultdict( lambda : [0, createSubCategory()] )
 	for filename in os.listdir(deviant_folder):
 		fullpath = os.path.join(deviant_folder, filename)
 		if os.path.isdir(fullpath):
 			continue
 		dom = xml.dom.minidom.parse(fullpath)
 		category = dom.getElementsByTagName("category")[0].childNodes[0].data
-		category = category.split('/')[0]
-		categories[category] += 1
+		
+		addToCategories(totalcategories, category)
+		addToCategories(usercategories, category)
+
+	if printperuser:
+		print('Analyzing deviant %s' % (deviant))
+		printCategories(usercategories, '\t', printsubcategories)
+		
+
+def addToCategories(categoriesdict, category):
+	category = category.split('/')
+	for c in category:
+		categoriesdict[c][0] += 1
+		categoriesdict = categoriesdict[c][1]
+
+def printCategories(categories, indent='\t', printsub=True):
+	if not categories:
+		return
 	categories = sorted(categories.iteritems(), key=itemgetter(1), reverse=True)
-	for v in categories:
-		print('\t%s: \t%d' % (v[0], v[1]))
+	for k, v in categories:
+		print '%s%s: %d' % (indent, k, v[0])
+		if printsub:
+			printCategories(v[1], indent = '%s\t' % (indent))
 
 def main():
+	global totalcategories, printsubcategories, printperuser
 	if len(sys.argv) < 2:
 		print('Usage: ./category_analyzer folder_name')
 		sys.exit(0)
 
 	folder_name = sys.argv[1]
 	
+	if len(sys.argv) > 2:
+		printperuser = eval(sys.argv[2])
+	if len(sys.argv) > 3:
+		printsubcategories = eval(sys.argv[3])
+		
 	for name in os.listdir(folder_name):
 		reportDeviant(folder_name, name)
 
+	if not printperuser:
+		printCategories(totalcategories, '', printsubcategories)
 
 if __name__ == '__main__':
 	main()
