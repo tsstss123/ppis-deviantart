aa75ec9f028f79feef0be2a983d2ab95bef6227e
QuankiQuanki <QuankiQuanki@6479cd68-e7ad-26bc-137c-6250a2daf512>
2010-06-18 12:29:15 +0000

    The extra new Classifier Code of 18 June

---
+++ Extra New Classifier Code 18 June/KNN.m
@@ -0,0 +1,45 @@
+function [ ClassStruct ] = KNN( data, R, crossfolds )
+    %KNN Classifier
+    Classifiername='KNN_Classifier';
+    Kmax=1;
+
+    knnstruct=struct(Classifiername,[]);
+    
+    for j=1:2:Kmax
+       disp(['Parameter K: ' int2str(j)]);
+       errorvector=zeros(1,crossfolds);
+       fnvector=zeros(1,crossfolds);
+       fpvector=zeros(1,crossfolds);
+       tpvector=zeros(1,crossfolds);
+       tnvector=zeros(1,crossfolds);
+       fvector=zeros(1,crossfolds);
+       precisionvector=zeros(1,crossfolds);
+       recallvector=zeros(1,crossfolds);
+       
+       for p=1:crossfolds
+            currentvalidationset=data(R==p,:);
+            currenttrainset=data(R~=p,:);
+            W=knnc(currenttrainset,j);
+            errorvector(p)=testc(currentvalidationset,W);
+            [fnvector(p),fpvector(p)]=testc(currentvalidationset,W,'FN','Positive');
+            [tpvector(p),tnvector(p)]=testc(currentvalidationset,W,'TP','Positive');
+            fvector(p)=testc(currentvalidationset,W,'F');
+            [precisionvector(p), recallvector(p)]=testc(currentvalidationset,W,'precision','Positive');
+       end
+       
+       parametername=['K' int2str(j)];
+
+       knnstruct.(Classifiername).(parametername).('Parameter_K')=j;
+       knnstruct.(Classifiername).(parametername).('errorrate')=mean(errorvector);
+       knnstruct.(Classifiername).(parametername).('fn')=mean(fnvector);
+       knnstruct.(Classifiername).(parametername).('fp')=mean(fpvector);
+       knnstruct.(Classifiername).(parametername).('tp')=mean(tpvector);
+       knnstruct.(Classifiername).(parametername).('tn')=mean(tnvector);
+       knnstruct.(Classifiername).(parametername).('f')=mean(fvector);
+       knnstruct.(Classifiername).(parametername).('precision')=mean(precisionvector);
+       knnstruct.(Classifiername).(parametername).('recall')=mean(recallvector);
+
+    end
+    ClassStruct=knnstruct;
+end
+
---
+++ Extra New Classifier Code 18 June/filterArtists.m
@@ -0,0 +1,10 @@
+function [ output_args ] = filterArtists( data, artists )
+%FILTERARTISTS Summary of this function goes here
+%   Detailed explanation goes here
+classnames=getlablist(data);
+getnlab(data);
+
+for i=1:
+
+end
+
