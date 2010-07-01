894a0a5ea4736803d96eddd8f5c7fcaeb4188b78
davide.modolo <davide.modolo@6479cd68-e7ad-26bc-137c-6250a2daf512>
2010-07-01 12:01:34 +0000

    files deleted

--- Final Version/Feature Extraction/src/getSimilaritySalMapEntropy.m
+++
@@ -1,7 +0,0 @@
-function [sim] = getSimilaritySalMapHist(img1_name, img2_name)
-
-    img1 = getSaliencyMap(img1_name);
-    img2 = getSaliencyMap(img2_name);
-    
-    sim = Entropy(img1) - Entropy(img2);
-end
